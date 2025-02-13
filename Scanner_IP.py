87121b734a33c45046746416b7e7278b9587dc69
Aiswor Gracia <graciarodriguezaiswor@gmail.com>
2025-02-13 21:46:27 +0100

    Add files via upload

---
+++ Scanner_IP.py
@@ -0,0 +1,27 @@
+import os
+import ipaddress
+import concurrent.futures
+
+def hacer_ping(ip):
+    response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+    if response == 0:
+        print(f"{ip} está activa")
+
+def main():
+    print("Introduce la IP de red: ")
+    ip_red = input()
+
+    print("Introduce la máscara en formato largo: ")
+    mask = input()
+
+    try:
+        red = ipaddress.ip_network(f"{ip_red}/{mask}", strict=False)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(hacer_ping, ip) for ip in red.hosts()]
+            for future in concurrent.futures.as_completed(futures):
+                pass
+    except ValueError as e:
+        print(f"Error en la red/máscara: {e}")
+
+if __name__ == "__main__":
+    main()
